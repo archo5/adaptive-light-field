3be9fc0b0443cb4559903168635f5bb3cee7efb5
archo5 <1819544+archo5@users.noreply.github.com>
2025-03-07 11:48:07 +0200

    basic grid acceleration, direction vector resolving, tweaks

--- Assets/LightVolSampler.shader
+++ Assets/LightVolSampler.shader
@@ -46,10 +46,18 @@ struct LPoint
 {
 	float3 pos;
 	float radius;
-	float4 color;
+	float3 color;
+	int vecCount;
 	LPVec vecs[27];
 };
 StructuredBuffer<LPoint> LightVolPoints;
+StructuredBuffer<uint> LightVolGrid;
+StructuredBuffer<uint> LightVolPtLists;
+float4 LightVolMin;
+float4 LightVolMax;
+int LightVolGridSizeX;
+int LightVolGridSizeY;
+int LightVolGridSizeZ;
 
 v2f vert(appdata v)
 {
@@ -61,21 +69,72 @@ v2f vert(appdata v)
 	return o;
 }
 
+float CalcWeight(LPoint LP, float3 wpos)
+{
+	float wt = 0, wtsum = 0;
+	float3 c2p = wpos - LP.pos;
+	float3 c2pdir = normalize(c2p);
+	float c2plen = length(c2p);
+	float c2plensq = c2plen * c2plen;
+	for (int i = 0; i < LP.vecCount; i++)
+	{
+		LPVec V = LP.vecs[i];
+		float lensq = V.len * V.len;
+		float dotf = saturate(dot(V.dir, c2pdir));
+
+		//dotf = pow(dotf, 4.);
+		dotf *= dotf;
+		dotf *= dotf;
+
+		//float distf = 1 - saturate(c2plensq / lensq);
+		float distf = 1 - saturate(c2plen / V.len);
+		//float distf = 1 / (0.01 + c2plensq / lensq);
+		//distf = pow(distf, 0.1);
+		float q = distf;
+		float w = dotf;
+		//retq = max(retq, q * w);
+		wt += w * q;
+		wtsum += w;
+	}
+	return wt / wtsum;
+}
+
 fixed4 frag(v2f input) : SV_Target
 {
 	fixed4 col = tex2D(_MainTex, input.uv);
 	// apply lighting
+	if (input.worldPos.x >= LightVolMin.x && input.worldPos.x <= LightVolMax.x &&
+		input.worldPos.y >= LightVolMin.y && input.worldPos.y <= LightVolMax.y &&
+		input.worldPos.z >= LightVolMin.z && input.worldPos.z <= LightVolMax.z)
 	{
 		float3 lighting = 0;
 		float totalw = 0;
 		uint count, dummy;
 		LightVolPoints.GetDimensions(count, dummy);
+#if 0
 		for (uint i = 0; i < count; i++)
 		{
+#else
+		int gridCellX = clamp(int((input.worldPos.x - LightVolMin.x) / (LightVolMax.x - LightVolMin.x) * LightVolGridSizeX), 0, LightVolGridSizeX - 1);
+		int gridCellY = clamp(int((input.worldPos.y - LightVolMin.y) / (LightVolMax.y - LightVolMin.y) * LightVolGridSizeY), 0, LightVolGridSizeY - 1);
+		int gridCellZ = clamp(int((input.worldPos.z - LightVolMin.z) / (LightVolMax.z - LightVolMin.z) * LightVolGridSizeZ), 0, LightVolGridSizeZ - 1);
+		int gridCellNum = gridCellX + gridCellY * LightVolGridSizeX + gridCellZ * LightVolGridSizeX * LightVolGridSizeY;
+		uint gridCellData = LightVolGrid[gridCellNum];
+		uint ploff = gridCellData & 0xfffff;
+		uint plcount = gridCellData >> 20;
+		for (uint p = 0; p < plcount; p++)
+		{
+			uint i = LightVolPtLists[p + ploff];
+#endif
 			LPoint LP = LightVolPoints[i];
-			if (length(LP.pos - input.worldPos) > LP.radius)
-				continue;
+			if (length(LP.pos - input.worldPos) > LP.radius) continue;
+#if 0
 			float w = 1 - length(LP.pos - input.worldPos) / LP.radius;
+			w *= w;
+#else
+			float w = CalcWeight(LP, input.worldPos);
+			w *= w;
+#endif
 			lighting += LP.color.rgb * w;
 			totalw += w;
 		}
--- Assets/Lighting.cs
+++ Assets/Lighting.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public struct LPoint
 {
 	public Vector3 pos;
@@ -15,17 +16,112 @@ public unsafe struct GBLPoint
 {
 	public Vector3 pos;
 	public float radius;
-	public Color color;
+	public Vector3 color;
+	public int vecCount;
 	public fixed float vecs[27 * 4];
+};
+
+[System.Serializable]
+public struct AABB3f
+{
+	public float x0, y0, z0;
+	public float x1, y1, z1;
+
+	public AABB3f GetCell(int x, int y, int z, Vector3Int size)
+	{
+		return new AABB3f()
+		{
+			x0 = Mathf.LerpUnclamped(x0, x1, x / (float) size.x),
+			y0 = Mathf.LerpUnclamped(y0, y1, y / (float) size.y),
+			z0 = Mathf.LerpUnclamped(z0, z1, z / (float) size.z),
+			x1 = Mathf.LerpUnclamped(x0, x1, (x + 1) / (float) size.x),
+			y1 = Mathf.LerpUnclamped(y0, y1, (y + 1) / (float) size.y),
+			z1 = Mathf.LerpUnclamped(z0, z1, (z + 1) / (float) size.z),
+		};
+	}
+	public Vector3 LimitPoint(Vector3 p)
+	{
+		if (p.x < x0) p.x = x0; else if (p.x > x1) p.x = x1;
+		if (p.y < y0) p.y = y0; else if (p.y > y1) p.y = y1;
+		if (p.z < z0) p.z = z0; else if (p.z > z1) p.z = z1;
+		return p;
+	}
+	public bool IntersectsSphere(Vector3 pos, float radius)
+	{
+		Vector3 cp = LimitPoint(pos);
+		return (pos - cp).magnitude < radius;
+	}
 };
 
 [ExecuteInEditMode]
 public class Lighting : MonoBehaviour
 {
-	public static List<LPoint> lpoints = new List<LPoint>();
-
+	// point generation
+	[HideInInspector]
+	public List<LPoint> lpoints = new List<LPoint>();
 	public float stepSize = 1;
 	public int extStepCount = 5;
+
+	// point acceleration
+	public AABB3f gridAABB = new AABB3f() { x0 = -5, y0 = -1, z0 = -5, x1 = 5, y1 = 3, z1 = 5 };
+	public float gridCellSize = 1;
+	[HideInInspector]
+	public Vector3Int gridSize;
+	// the grid referring to point lists below
+	// encoding: 20b offset, 12b count
+	[HideInInspector]
+	public int[] gridListOffsets = new int[0];
+	// the point lists of the grid, indexed by the grid above
+	[HideInInspector]
+	public List<int> gridPointLists = new List<int>();
+
+	// light baking
+	public Color ambientColor = new Color(0.1f, 0.2f, 0.4f);
+
+	// debug drawing
+	public bool drawSpheres = true;
+	public bool drawVectors = true;
+
+	void BuildGrid()
+	{
+		gridSize = new Vector3Int()
+		{
+			x = Mathf.CeilToInt((gridAABB.x1 - gridAABB.x0) / gridCellSize),
+			y = Mathf.CeilToInt((gridAABB.y1 - gridAABB.y0) / gridCellSize),
+			z = Mathf.CeilToInt((gridAABB.z1 - gridAABB.z0) / gridCellSize),
+		};
+
+		gridListOffsets = new int[gridSize.x * gridSize.y * gridSize.z];
+		gridPointLists.Clear();
+
+		int maxCountPerCell = 0;
+		for (int z = 0; z < gridSize.z; z++)
+		{
+			for (int y = 0; y < gridSize.y; y++)
+			{
+				for (int x = 0; x < gridSize.x; x++)
+				{
+					AABB3f cell = gridAABB.GetCell(x, y, z, gridSize);
+					int offset = gridPointLists.Count;
+					for (int i = 0; i < lpoints.Count; i++)
+					{
+						var lp = lpoints[i];
+						if (cell.IntersectsSphere(lp.pos, lp.radius))
+						{
+							gridPointLists.Add(i);
+						}
+					}
+					int count = gridPointLists.Count - offset;
+					gridListOffsets[x + y * gridSize.x + z * gridSize.x * gridSize.y] = offset | (count << 20);
+					maxCountPerCell = Mathf.Max(maxCountPerCell, count);
+				}
+			}
+		}
+		Debug.LogFormat("Built grid: {0}x{1}x{2}, max # per cell: {3}, avg # per cell: {4}",
+			gridSize.x, gridSize.y, gridSize.z,
+			maxCountPerCell,
+			gridPointLists.Count / (gridSize.x * gridSize.y * gridSize.z));
+	}
 
 	[ContextMenu("Generate points")]
 	void GeneratePoints()
@@ -161,7 +257,7 @@ public class Lighting : MonoBehaviour
 					continue;
 				Vector3 vec = lpoints[j].pos - lp.pos;
 				float len = vec.magnitude;
-				if (len < 1.5f)
+				if (len < stepSize * 1.1f)
 				{
 					radius = Mathf.Max(radius, len);
 					lp.vecs.Add(vec);
@@ -174,6 +270,8 @@ public class Lighting : MonoBehaviour
 		Debug.LogFormat("Points generated: {0}", lpoints.Count);
 		Debug.LogFormat("Largest nearby count: {0}", largestNearbyCount);
 
+		BuildGrid();
+
 		// generate colors
 		for (int i = 0; i < lpoints.Count; i++)
 		{
@@ -182,11 +280,63 @@ public class Lighting : MonoBehaviour
 			lpoints[i] = lp;
 		}
 
-		// kill the graphics buffer
-		if (gbuf != null)
+		DeleteGraphicsBuffers();
+	}
+
+	[ContextMenu("Bake lighting")]
+	void BakeLighting()
+	{
+		var lights = FindObjectsByType<Light>(FindObjectsSortMode.None);
+		Physics.queriesHitBackfaces = true;
+		for (int i = 0; i < lpoints.Count; i++)
 		{
-			gbuf.Dispose();
-			gbuf = null;
+			var lp = lpoints[i];
+			lp.color = ambientColor;
+			foreach (var L in lights)
+			{
+				if (L.type == LightType.Directional)
+				{
+					Vector3 dir = L.transform.forward;
+					if (!Physics.Raycast(lp.pos - dir * 1000.001f, dir, 1000))
+					{
+						lp.color += L.color;
+					}
+				}
+				else if (L.type == LightType.Rectangle)
+				{
+					Vector3 dir = L.transform.forward;
+					Vector3 lightPos = L.transform.position;
+					Vector3 l2p = lp.pos - L.transform.position;
+					Vector3 l2pd = l2p.normalized;
+					float dist = l2p.magnitude;
+					if (!Physics.Raycast(lightPos + l2pd * 0.001f, l2pd, dist - 0.002f))
+					{
+						float amount = Mathf.Clamp01(1 - dist / L.range) * Mathf.Clamp01(Vector3.Dot(dir, l2pd));
+						lp.color += L.color * amount;
+					}
+				}
+			}
+			lpoints[i] = lp;
+		}
+		DeleteGraphicsBuffers();
+	}
+
+	void DeleteGraphicsBuffers()
+	{
+		if (gbufPoints != null)
+		{
+			gbufPoints.Dispose();
+			gbufPoints = null;
+		}
+		if (gbufGrid != null)
+		{
+			gbufGrid.Dispose();
+			gbufGrid = null;
+		}
+		if (gbufPtLists != null)
+		{
+			gbufPtLists.Dispose();
+			gbufPtLists = null;
 		}
 	}
 
@@ -197,37 +347,55 @@ public class Lighting : MonoBehaviour
 
 	void OnDrawGizmos()
 	{
-		Gizmos.color = Color.red;
-		foreach (var lp in lpoints)
+		Gizmos.DrawWireCube(new Vector3()
 		{
-			Gizmos.color = lp.color;
-			Gizmos.DrawSphere(lp.pos, 0.05f);
+			x = (gridAABB.x0 + gridAABB.x1) / 2,
+			y = (gridAABB.y0 + gridAABB.y1) / 2,
+			z = (gridAABB.z0 + gridAABB.z1) / 2,
+		},
+		new Vector3()
+		{
+			x = gridAABB.x1 - gridAABB.x0,
+			y = gridAABB.y1 - gridAABB.y0,
+			z = gridAABB.z1 - gridAABB.z0,
+		});
+		if (drawSpheres)
+		{
+			Gizmos.color = Color.red;
+			foreach (var lp in lpoints)
+			{
+				Gizmos.color = lp.color;
+				Gizmos.DrawSphere(lp.pos, 0.05f);
+			}
 		}
-		Gizmos.color = Color.yellow;
-		foreach (var lp in lpoints)
+		if (drawVectors)
 		{
-			foreach (var vec in lp.vecs)
-				Gizmos.DrawLine(lp.pos, lp.pos + vec * 0.45f);
+			Gizmos.color = Color.yellow;
+			foreach (var lp in lpoints)
+			{
+				foreach (var vec in lp.vecs)
+					Gizmos.DrawLine(lp.pos, lp.pos + vec * 0.45f);
+			}
 		}
 	}
 
 	void OnDestroy()
 	{
-		if (gbuf != null)
-		{
-			gbuf.Dispose();
-			gbuf = null;
-		}
+		DeleteGraphicsBuffers();
 	}
 
-	GraphicsBuffer gbuf;
+	GraphicsBuffer gbufPoints;
+	GraphicsBuffer gbufGrid;
+	GraphicsBuffer gbufPtLists;
 	//unsafe void OnPreRender()
 	unsafe void LateUpdate()
 	{
 		if (lpoints.Count != 0)
 		{
-			if (gbuf == null || gbuf.count != lpoints.Count)
+			if (gbufPoints == null || gbufPoints.count != lpoints.Count)
 			{
+				if (gbufPoints != null)
+					gbufPoints.Dispose();
 				var gblps = new List<GBLPoint>(lpoints.Count);
 				foreach (var lp in lpoints)
 				{
@@ -235,7 +403,8 @@ public class Lighting : MonoBehaviour
 					{
 						pos = lp.pos,
 						radius = lp.radius,
-						color = lp.color,
+						color = new Vector3() { x = lp.color.r, y = lp.color.g, z = lp.color.b },
+						vecCount = Mathf.Min(lp.vecs.Count, 27)
 					};
 					for (int i = 0; i < lp.vecs.Count && i < 27; i++)
 					{
@@ -248,11 +417,39 @@ public class Lighting : MonoBehaviour
 					}
 					gblps.Add(gblp);
 				}
-				gbuf = new GraphicsBuffer(GraphicsBuffer.Target.Structured, lpoints.Count, System.Runtime.InteropServices.Marshal.SizeOf<GBLPoint>());
-				gbuf.SetData(gblps);
+				gbufPoints = new GraphicsBuffer(GraphicsBuffer.Target.Structured, lpoints.Count, System.Runtime.InteropServices.Marshal.SizeOf<GBLPoint>());
+				gbufPoints.SetData(gblps);
 			}
 			//Debug.LogFormat("Count:{0}", gbuf.count);
-			Shader.SetGlobalBuffer("LightVolPoints", gbuf);
+			Shader.SetGlobalBuffer("LightVolPoints", gbufPoints);
+		}
+
+		if (gridSize != Vector3Int.zero)
+		{
+			int gridCellCount = gridSize.x * gridSize.y * gridSize.z;
+			if (gbufGrid == null || gbufGrid.count != gridCellCount)
+			{
+				if (gbufGrid != null)
+					gbufGrid.Dispose();
+				gbufGrid = new GraphicsBuffer(GraphicsBuffer.Target.Structured, gridCellCount, 4);
+			}
+			gbufGrid.SetData(gridListOffsets);
+			Shader.SetGlobalBuffer("LightVolGrid", gbufGrid);
+
+			if (gbufPtLists == null || gbufPtLists.count != gridPointLists.Count)
+			{
+				if (gbufPtLists != null)
+					gbufPtLists.Dispose();
+				gbufPtLists = new GraphicsBuffer(GraphicsBuffer.Target.Structured, gridPointLists.Count, 4);
+			}
+			gbufPtLists.SetData(gridPointLists);
+			Shader.SetGlobalBuffer("LightVolPtLists", gbufPtLists);
+
+			Shader.SetGlobalVector("LightVolMin", new Vector4() { x = gridAABB.x0, y = gridAABB.y0, z = gridAABB.z0 });
+			Shader.SetGlobalVector("LightVolMax", new Vector4() { x = gridAABB.x1, y = gridAABB.y1, z = gridAABB.z1 });
+			Shader.SetGlobalInteger("LightVolGridSizeX", gridSize.x);
+			Shader.SetGlobalInteger("LightVolGridSizeY", gridSize.y);
+			Shader.SetGlobalInteger("LightVolGridSizeZ", gridSize.z);
 		}
 	}
 }
